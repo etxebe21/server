18d003e1f4c847596ab62fb25d0d59cdeae342e0
= <=>
2023-02-16 00:42:15 +0100

    cambios en highscore para usar tabla highscores

--- model/Highscores.php
+++ model/Highscores.php
@@ -6,7 +6,7 @@ class Highscores extends ModelBase
     function __construct()
     {
         //Inicializamos el nombre de la tabla
-        $this->table_name = 'highscoreVista';
+        $this->table_name = 'highscores';
 
         //Llamamos al constructor de la clase ModelBase
         parent::__construct();
